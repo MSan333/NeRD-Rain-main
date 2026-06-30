49b8a8551b6f94b547499f37006cdc0e4fbbc275
guoshuaile <guoshuaile.gsl@alibaba-inc.com>
2026-06-30 11:03:13 +0800

    fix: 更新train_ddp.sh添加conda激活和screen说明

--- train_ddp.sh
+++ train_ddp.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-#SBATCH -p gpu20
-#SBATCH -t 7-00:00:00
-
-#conda activate pytorch
 
 # 4卡 3090 DDP 训练
 # 每卡 bs=1, 总 bs=4, lr=4e-4 (线性缩放)
+# 使用方法: screen -S exp2 && bash train_ddp.sh
+
+# 激活conda环境
+source activate nerd
+
 CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --nproc_per_node=4 \
     train.py \
     --session "DDP_4GPU" \
     --batch_size 1 \
     --num_epochs 3000 \
     --patch_size 256 \
-    | tee logs_Rain200L_ddp.txt
+    2>&1 | tee logs_Rain200L_ddp.txt
